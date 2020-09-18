696232d1492ec751603e61e8ee15a7fd859b4b8e
Jherson Haryson <jherson_k-f@hotmail.com>
2020-09-18 08:26:53 -0300

    add CharacterCarousel component

---
+++ src/components/CharacterCarousel/index.js
@@ -0,0 +1,28 @@
+import React from 'react' 
+import Flickity from 'react-flickity-component'
+ 
+const flickityOptions = {
+    initialIndex: 0,
+    cellAlign: 'left',
+    freeScroll: true,
+    autoPlay: 4000,
+    prevNextButtons: false,
+    friction: .07,
+    selectedAttraction: .01
+
+}
+ 
+function CharacterCarousel({items = []}) {
+  if(!items.length) return  
+  return (
+    <Flickity
+      options={flickityOptions}
+      style={{width: "100%"}}
+      static
+    >
+      {items.map(item=>item)}
+    </Flickity>
+  )
+}
+
+export default CharacterCarousel;
