8da2a155498e7d4a7e37805b94d06af22dcc0c5d
Jherson Haryson <jherson_k-f@hotmail.com>
2020-09-20 07:39:11 -0300

    change carrousell to dont drag

--- src/components/FooterCarousel/index.js
+++ src/components/FooterCarousel/index.js
@@ -1,29 +1,28 @@
-import React from 'react' 
-import Flickity from 'react-flickity-component'
- 
-const flickityOptions = {
-    initialIndex: 0,
-    cellAlign: 'left',
-    freeScroll: true,
-    autoPlay: 4000,
-    prevNextButtons: false,
-    friction: .07,
-    selectedAttraction: .01,
-    contain: true,
+import React from "react";
+import Flickity from "react-flickity-component";
 
-}
- 
-function FooterCarousel({items = []}) {
-  if(!items.length) return null
+function FooterCarousel({ items = [] }) {
+  if (!items.length) return null;
+
+  const flickityOptions = {
+    initialIndex: 0,
+    cellAlign: "left",
+    // freeScroll: true,
+    draggable: false,
+    autoPlay: 4000,
+    prevNextButtons: true,
+    friction: 0.07,
+    selectedAttraction: 0.01,
+    contain: true,
+    groupCells: 3,
+    // wrapAround: true,
+    imagesLoaded: false,
+  };
   return (
-    <Flickity
-      options={flickityOptions}
-      style={{width: "100%"}}
-      static
-    >
-      {items.map(item=>item)}
+    <Flickity options={flickityOptions} style={{ width: "100%" }} static>
+      {items.map((item) => item)}
     </Flickity>
-  )
+  );
 }
 
 export default FooterCarousel;
