1fa517f7d19425cf2cba68224373567659c98cb6
Jherson Haryson <jherson_k-f@hotmail.com>
2020-09-20 05:18:27 -0300

    add CharacterComicGalery component

---
+++ src/components/CharacterComicsGallery/index.js
@@ -0,0 +1,118 @@
+import React, { useState, useEffect } from "react";
+import { Theme } from "../../models/HerosModel";
+import ViewModel from "../../models/ViewModel";
+import CharacterService from "../../services/CharacterService";
+import ComicsService from "../../services/ComicsService";
+import ComicsCarousel from "../ComicsCarousel";
+import { LoadButton, MainComicsList, ItemComicsList } from "../ComicsList";
+import Loader from "../Loader";
+import { ComicCol, ComicRow, PaginatorInfo } from "./styles";
+
+function CharacterComicsGallery({
+  query = "",
+  queryIsId = false,
+  isComic = false,
+  onSearchEnd = () => {},
+  handleSelect = () => {},
+}) {
+  const [results, setResults] = useState([]);
+  const [index, setIndex] = useState(0);
+  const [limit, setlimit] = useState(10);
+  const [offset, setOffset] = useState(0);
+  const [count, setCount] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setResults([]);
+    setIndex(0);
+    setlimit(10);
+    setOffset(0);
+    setCount(0);
+    setTotal(0);
+    setLoading(false);
+  }, [query]);
+
+  useEffect(() => {
+    const fetch = async () => {
+      setLoading(true);
+
+      const response = isComic
+        ? await new ComicsService().getCharactersResourceByComicId(
+            query,
+            index * limit,
+            limit
+          )
+        : await new CharacterService().getComicsResourceByCharcterId(
+            query,
+            index * limit,
+            limit
+          );
+
+      console.log(response);
+      const { offset = 0, results = [], total = 0, count } = response;
+      onSearchEnd();
+      setOffset(offset);
+      setCount(count);
+      setTotal(total);
+      setResults(results);
+      setLoading(false);
+    };
+    fetch();
+  }, [index]);
+
+  const handleChange = (isNext) => {
+    if (isNext) {
+      // verificar se o index > max
+      setIndex(index + 1 < total ? index + 1 : index);
+    } else {
+      setIndex(index > 0 ? index - 1 : 0);
+    }
+  };
+
+  const buildGallery = (items) => {
+    let gallery = [];
+
+    if (index > 0) gallery.push(<LoadButton onClick={handleChange} key={11} />);
+
+    gallery = [...gallery, ...items];
+
+    if (index + 1 < total / limit)
+      gallery.push(<LoadButton next onClick={handleChange} key={22} />);
+
+    return gallery;
+  };
+
+  return (
+    <ComicCol>
+      {/* {JSON.stringify(results)} */}
+      <ComicRow>
+        {query && results.length > 0 && (
+          <PaginatorInfo side="flex-start">
+            {isComic ? `${total} character appears in this comic` : `All ${total} comics this character appears`}
+          </PaginatorInfo>
+        )}
+      </ComicRow>
+      <ComicRow style={{ minHeight: "200px" }}>
+        {loading ? (
+          <Loader />
+        ) : (
+          <ComicsCarousel
+            items={buildGallery(
+              results.map((result) => ItemComicsList(result, handleSelect))
+            )}
+          />
+        )}
+      </ComicRow>
+      <ComicRow>
+        {query && !(total == 0 && query) && (
+          <PaginatorInfo side="flex-end">
+            {index + 1} / {Math.ceil(total / limit)}
+          </PaginatorInfo>
+        )}
+      </ComicRow>
+    </ComicCol>
+  );
+}
+
+export default CharacterComicsGallery;
