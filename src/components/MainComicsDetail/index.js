49e974dd4433e55bcf96260628308a9e39f5868d
Jherson Haryson <jherson_k-f@hotmail.com>
2020-09-20 05:17:04 -0300

    smooth scroll to id's component in screen

--- src/components/MainComicsDetail/index.js
+++ src/components/MainComicsDetail/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FiArrowLeft } from "react-icons/fi";
 import { Link } from "react-router-dom";
 import Names from "../Names";
@@ -12,11 +12,21 @@ import {
   TitleBar,
   ComicCover,
   ResumeIsNotAvaliable,
+  StickPosition,
 } from "./styles";
 import { MiniComicsList } from "../ComicsList";
 import ReadMoreButton from "../ReadMoreButton";
+import ComicsGallery from "../ComicsGallery";
+import { Theme } from "../../models/HerosModel";
+import ViewModel from "../../models/ViewModel";
+import CharacterComicsGallery from "../CharacterComicsGallery";
 
-function MainComicsDetail({ comic }) {
+function MainComicsDetail({ comic, handleSelect }) {
+  const [update, setUpdate] = useState(false);
+  useEffect(() => {
+    setUpdate(true);
+    setTimeout(() => setUpdate(false), 500);
+  }, [comic]);
   return (
     <Container
       id="result"
@@ -39,94 +49,172 @@ function MainComicsDetail({ comic }) {
           marginBottom: 0,
           paddingBbottom: 0,
           paddingTop: "45px",
+          flexDirection: "column",
         }}
       >
-        <Box>
-          <TitleBar>
-            <ComicTitle>{comic.title}</ComicTitle>
-          </TitleBar>
+        <Container>
+          <Box>
+            <TitleBar>
+              <ComicTitle>{comic.title || comic.name}</ComicTitle>
+            </TitleBar>
 
-          <Flex>
-            <div>
-              <ComicCover
-                src={`${comic.thumbnail.path}/portrait_incredible.jpg`}
-                alt="comics"
-                style={{ width: 216 * 1.5, height: 346 * 1.5 }}
+            <Flex>
+              <StickPosition>
+                <ComicCover
+                  src={`${comic.thumbnail.path}/portrait_incredible.jpg`}
+                  alt="comics"
+                  style={{ width: 216 * 1.5, height: 346 * 1.5 }}
+                />
+                {!!comic?.urls?.length && (
+                  <a href={comic.urls[0].url} target="blank">
+                    <ReadMoreButton fill="#000" width={216 * 1.5} />
+                  </a>
+                )}
+              </StickPosition>
+
+              <InformationBox>
+                {!!comic?.title && (
+                  <>
+                    <Title>TITLE</Title>
+                    <p>{comic.title}</p>
+                  </>
+                )}
+
+                {!!comic?.name && (
+                  <>
+                    <Title>NAME</Title>
+                    <p>{comic.name}</p>
+                  </>
+                )}
+
+                <Title>RESUME</Title>
+                {!!comic?.description ? (
+                  <p>{comic.description}</p>
+                ) : (
+                  <ResumeIsNotAvaliable>Not avaiable</ResumeIsNotAvaliable>
+                )}
+
+                {!!comic?.creators?.length && (
+                  <>
+                    <Title>CREATORS</Title>
+                    <ul>
+                      {comic?.creators?.map(({ name, role }, index) => (
+                        <li key={index}>
+                          {name}
+                          <span> ({role})</span>
+                        </li>
+                      ))}
+                    </ul>
+                  </>
+                )}
+
+                {!!comic?.stories?.length && (
+                  <>
+                    <Title>STORIES</Title>
+                    <ul>
+                      {comic?.stories?.map(({ name, type }, index) => (
+                        <li key={index}>
+                          {name}
+                          <span> ({type})</span>
+                        </li>
+                      ))}
+                    </ul>
+                  </>
+                )}
+
+                {!!comic?.characters?.length && (
+                  <>
+                    <Title>CHARACTERS</Title>
+                    <ul>
+                      {comic?.characters?.map(({ name }, index) => (
+                        <li key={index}>{name}</li>
+                      ))}
+                    </ul>
+                  </>
+                )}
+
+                {!!comic?.dates?.length && (
+                  <>
+                    <Title>ONSALE DATE</Title>
+                    <p>{new Date(comic?.dates[0]["date"]).toDateString()}</p>
+                  </>
+                )}
+
+                {!!comic?.urls?.length && (
+                  <>
+                    <Title>LINKS</Title>
+                    <ul>
+                      {comic?.urls?.map(({ url, type }, index) => (
+                        <li key={index}>
+                          <a href={url} target="blank">
+                            {type?.toUpperCase()}
+                          </a>
+                        </li>
+                      ))}
+                    </ul>
+                  </>
+                )}
+              </InformationBox>
+            </Flex>
+          </Box>
+          <Box
+            style={{
+              justifyContent: "center",
+              marginTop: "67px",
+              maxHeight: "700px",
+            }}
+          >
+            <div style={{ alignSelf: "flex-end" }}>
+              <Names
+                name={comic?.title?.split(" ")[0] || comic?.name?.split(" ")[0]}
+                left={"flex-end"}
               />
-              <ReadMoreButton fill="#000" width={216 * 1.5} />
-            </div>
 
-            <InformationBox>
-              {!!comic?.title && (
+              {!!comic?.images?.length && (
                 <>
-                  <Title>TITLE</Title>
-                  <p>{comic.title}</p>
+                  <MiniComicsList list={comic.images} />
+                  <Title
+                    style={{
+                      marginRight: 10,
+                      marginTop: 15,
+                      textAlign: "right",
+                    }}
+                  >
+                    Images
+                  </Title>
                 </>
               )}
 
-              <Title>RESUME</Title>
-              {!!comic?.description ? (
-                <p>{comic.description}</p>
-              ) : (
-                <ResumeIsNotAvaliable>Not avaiable</ResumeIsNotAvaliable>
-              )}
-
-              {!!comic?.creators?.length && (
-                <>
-                  <Title>CREATORS</Title>
-                  <ul>
-                    {comic?.creators?.map(({ name, role }, index) => (
-                      <li key={index}>
-                        {name}
-                        <span> ({role})</span>
-                      </li>
-                    ))}
-                  </ul>
-                </>
-              )}
-
-              {!!comic?.characters?.length && (
-                <>
-                  <Title>CHARACTERS</Title>
-                  <ul>
-                    {comic?.characters?.map(({ name }, index) => (
-                      <li key={index}>{name}</li>
-                    ))}
-                  </ul>
-                </>
-              )}
-
-              {!!comic?.dates?.length && (
-                <>
-                  <Title>ONSALE DATE</Title>
-                  <p>{new Date(comic?.dates[0]["date"]).toDateString()}</p>
-                </>
-              )}
-            </InformationBox>
-          </Flex>
-        </Box>
-        <Box
-          style={{
-            justifyContent: "center",
-            marginTop: "67px",
-            maxHeight: "700px",
-          }}
-        >
-          <div style={{ alignSelf: "flex-end" }}>
-            <Names name={comic?.title?.split(" ")[0]} left={"flex-end"} />
-
-            {!!comic?.images?.length && (
-              <>
-                <MiniComicsList list={comic.images} />
-                <Title
-                  style={{ marginRight: 10, marginTop: 15, textAlign: "right" }}
+              {!!comic?.events?.length && (
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "flex-end",
+                    marginTop: "32px",
+                  }}
                 >
-                  Images
-                </Title>
-              </>
-            )}
-          </div>
-        </Box>
+                  <div style={{ minWidth: "200px" }}>
+                    <Title style={{ marginLeft: "-16px" }}>EVENTS</Title>
+                    <ul>
+                      {comic?.events?.map(({ name }, index) => (
+                        <li key={index}>{name}</li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              )}
+            </div>
+          </Box>
+        </Container>
+        {!update && (
+          <CharacterComicsGallery
+            isComic={!!comic.title}
+            queryIsId={true}
+            query={comic.id}
+            filter={ViewModel.CHARACTER}
+            handleSelect={handleSelect}
+          />
+        )}
       </Container>
     </Container>
   );
--- src/components/MainComicsDetail/styles.js
+++ src/components/MainComicsDetail/styles.js
@@ -1,95 +1,101 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const Container = styled.div`
-    display: flex; 
-    padding: 0px 50px;
-    margin-bottom: 10px;
+  display: flex;
+  padding: 0px 50px;
+  margin-bottom: 10px;
 `;
 
 export const SearchContainerField = styled.div`
-    display: flex; 
-    padding: 0px 50px;
-    flex-direction: column;
-    overflow: hidden;
+  display: flex;
+  padding: 0px 50px;
+  flex-direction: column;
+  overflow: hidden;
 `;
 
 export const Box = styled.div`
-    flex: 1;
+  flex: 1;
 `;
 
 export const Flex = styled.div`
-    display: flex;
+  display: flex;
+`;
+
+export const StickPosition = styled.div`
+  position: -webkit-sticky;
+  position: sticky;
+  top: 16px;
+  z-index: 2;
+  height: fit-content;
+  padding-bottom: 16px;
 `;
 
 export const ComicCover = styled.img`
-    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
-    border-radius: 3px;
-    transition: transform .2s;
-    margin: 0 10px;
-    position: -webkit-sticky;
-    position: sticky;
-    top: 16px;
-    z-index: 2;
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  border-radius: 3px;
+  transition: transform 0.2s;
+  margin: 0 10px;
 
-    &:hover {
-        transform: scale(1.15);
-    }
+  &:hover {
+    transform: scale(1.15);
+  }
 `;
 
 export const TitleBar = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 30px;
+
+  a {
     display: flex;
     align-items: center;
-    margin-bottom: 30px;
-
-    a {
-        display: flex;
-        align-items: center;
-        color: #fff;
-        font-size: 32px;
-        margin-right: 10px;
-        margin-left: -5px;
-    }
+    color: #fff;
+    font-size: 32px;
+    margin-right: 10px;
+    margin-left: -5px;
+  }
 `;
 
 export const ComicTitle = styled.h1`
-    color: #fff;
+  color: #fff;
 `;
 
 export const ResumeIsNotAvaliable = styled.h1`
-    color: #513d3d;
-    font-size: 12px;
-    font-weight: 900;
-    padding-left: 8px;
-    padding-right: 8px;
-    padding-top: 4px;
-    padding-bottom: 4px;
-    border-radius: 5px;
-    background-color: #fff;
-    width: 90px;
-    margin-bottom: 35px;
-    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  color: #513d3d;
+  font-size: 12px;
+  font-weight: 900;
+  padding-left: 8px;
+  padding-right: 8px;
+  padding-top: 4px;
+  padding-bottom: 4px;
+  border-radius: 5px;
+  background-color: #fff;
+  width: 90px;
+  margin-bottom: 35px;
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
 `;
 
 export const InformationBox = styled.div`
-    flex: 1;
-    margin-left: 24px;
-    p, ul {
-        margin-bottom: 40px;
-        color: #fff;
-    }
-    span {
-        font-size: 12px;
-    }
+  flex: 1;
+  margin-left: 24px;
+  p,
+  ul {
+    margin-bottom: 40px;
+    color: #fff;
+  }
+  span {
+    font-size: 12px;
+  }
 
-    li {
-        margin-left: 16px;
-    }
+  li {
+    margin-left: 16px;
+  }
 `;
 
 export const Title = styled.h2`
-    color: #fff;
-    text-transform: uppercase;
-    font-size: 18px;
-    font-weight: 700;
-    margin-bottom: 10px;
+  color: #fff;
+  text-transform: uppercase;
+  font-size: 18px;
+  font-weight: 700;
+  margin-bottom: 10px;
 `;
