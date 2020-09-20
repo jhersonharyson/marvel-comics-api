e1c90e64305acee57a4b0b714c253f5260c75f21
Jherson Haryson <jherson_k-f@hotmail.com>
2020-09-20 05:24:39 -0300

    add services resources for search comic by character and character by comic

--- src/services/CharacterService.js
+++ src/services/CharacterService.js
@@ -4,24 +4,142 @@ import axios from "./../http";
 export default class CharacterService {
   URL = {
     CHARACTERS: "characters",
+    COMICS: "comics",
   };
 
   getCharacter(characterId) {
     return axios.get(`${this.URL.CHARACTERS}/${characterId}`);
   }
 
+  getCharacterComics(characterId = "", offset = 0, limit = 10) {
+    return axios.get(
+      `${this.URL.CHARACTERS}/${characterId}/${this.URL.COMICS}?offset=${offset}&limit=${limit}`
+    );
+  }
+
+  getCharacterByNameStartsWith(name = "", offset = 0, limit = 10) {
+    return axios.get(
+      `${this.URL.CHARACTERS}?nameStartsWith=${name}&offset=${offset}&limit=${limit}`
+    );
+  }
+
   async getCharacterDescription(characterId) {
     try {
       const response = await this.getCharacter(characterId);
-      console.log(response)
+      console.log(response);
       const {
         name = "",
         description = "",
-        comics: comics,
+        comics,
       } = response.data.data.results[0];
       return { name, description, comics: comics.items };
-    }catch(e){
-       
+    } catch (e) {}
+  }
+
+  async getCharacterResourceByNameStartsWith(
+    isCharacterId,
+    _name,
+    _offset = 0,
+    _limit = 10
+  ) {
+    try {
+      const response = isCharacterId
+        ? await this.getCharacter(_name)
+        : await this.getCharacterByNameStartsWith(_name, _offset, _limit);
+      console.log(response);
+      const { offset, limit, total, count } = response.data.data;
+      const list = response.data.data.results;
+
+      let listOfComics = list.map(
+        ({
+          id,
+          description = "",
+          images,
+          thumbnail,
+          pageCount,
+          name,
+          comics,
+          stories,
+          events,
+          urls,
+        }) => {
+          return {
+            id,
+            description,
+            images,
+            thumbnail,
+            pageCount,
+            name,
+            comics: comics.items,
+            stories: stories.items,
+            events: events.items,
+            urls,
+          };
+        }
+      );
+
+      return {
+        results: [...listOfComics],
+        offset,
+        limit,
+        total,
+        count,
+      };
+    } catch (e) {}
+  }
+
+  async getComicsResourceByCharcterId(characterId, _offset = 0, _limit = 10) {
+    try {
+      const response = await this.getCharacterComics(
+        characterId,
+        _offset,
+        _limit
+      );
+
+      console.log(response);
+      const { offset, limit, total, count } = response.data.data;
+      const list = response.data.data.results;
+
+      let listOfComics = list.map(
+        ({
+          id,
+          description = "",
+          images,
+          thumbnail,
+          pageCount,
+          title,
+          stories,
+          events = {},
+          urls,
+          characters,
+          creators,
+        }) => {
+          return {
+            id,
+            description,
+            images,
+            thumbnail,
+            pageCount,
+            title,
+            stories: stories.items,
+            urls,
+            characters: characters.items,
+            creators: creators.items,
+          };
+        }
+      );
+
+      console.log(listOfComics);
+
+      return {
+        results: [...listOfComics],
+        offset,
+        limit,
+        total,
+        count,
+      };
+    } catch (e) {
+      console.log(e);
     }
   }
 }
--- src/services/ComicsService.js
+++ src/services/ComicsService.js
@@ -2,6 +2,7 @@ import axios from "../http";
 
 export default class ComicsService {
   URL = {
+    CHARACTER: "characters",
     COMICS: "comics",
   };
 
@@ -9,22 +10,32 @@ export default class ComicsService {
     return axios.get(resourceUrl);
   }
 
-  getComicByStartWithTitle(title='', offset = 0, limit = 4) {
+  getComicByStartWithTitle(title = "", offset = 0, limit = 10) {
     return axios.get(
       `${this.URL.COMICS}?titleStartsWith=${title}&offset=${offset}&limit=${limit}`
     );
   }
 
-  getComicByComicId(comicId='') {
+  getComicByComicId(comicId = "") {
+    return axios.get(`${this.URL.COMICS}/${comicId}`);
+  }
+
+  getCharactersByComicId(comicId = "", offset = 0, limit = 10) {
     return axios.get(
-      `${this.URL.COMICS}/${comicId}`
+      `${this.URL.COMICS}/${comicId}/${this.URL.CHARACTER}?offset=${offset}&limit=${limit}`
     );
   }
 
-  async getComicResourceByStartWithTitle(isComicId = false, title='', _offset = 0, _limit = 4) {
-    
+  async getComicResourceByStartWithTitle(
+    isComicId = false,
+    title = "",
+    _offset = 0,
+    _limit = 4
+  ) {
     try {
-      const response = isComicId ? await this.getComicByComicId(title) : await this.getComicByStartWithTitle(title, _offset, _limit);
+      const response = isComicId
+        ? await this.getComicByComicId(title)
+        : await this.getComicByStartWithTitle(title, _offset, _limit);
       console.log(response);
       const { offset, limit, total, count } = response.data.data;
       const list = response.data.data.results;
@@ -39,6 +50,7 @@ export default class ComicsService {
           dates,
           pageCount,
           title,
+          urls = {},
         }) => {
           return {
             id,
@@ -48,6 +60,7 @@ export default class ComicsService {
             description,
             creators: creators.items,
             characters: characters.items,
+            urls: urls.items,
             dates,
             pageCount,
           };
@@ -61,7 +74,7 @@ export default class ComicsService {
         count,
       };
     } catch (e) {
-      console.log(e)
+      console.log(e);
     }
   }
 
@@ -77,19 +90,74 @@ export default class ComicsService {
         characters,
         dates,
         pageCount,
-        title,
+        name,
+        comics,
+        series,
+        urls = {},
       } = response.data.data.results[0];
 
       return {
         images,
         thumbnail,
-        title,
+        name,
         description,
         creators: creators.items,
         characters: characters.items,
+        urls: urls.items,
         dates,
         pageCount,
+        comics: comics.items,
+        series: comics.series,
       };
-    } catch (e) {}
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async getCharactersResourceByComicId(comicId) {
+    try {
+      const response = await this.getCharactersByComicId(comicId);
+      console.log(response);
+      const { offset, limit, total, count } = response.data.data;
+      const list = response.data.data.results;
+      let listOfComics = list.map(
+        ({
+          id,
+          description = "",
+          images,
+          thumbnail,
+          creators,
+          name,
+          urls = {},
+          stories = {},
+          series = {},
+          comics = {},
+          events = {}
+        }) => {
+          return {
+            id,
+            description,
+            images,
+            thumbnail,
+            creators: creators?.items,
+            stories: stories?.items,
+            series: series?.items,
+            events: events.items,
+            name,
+            urls: urls.items,
+            comics: comics.items,
+          };
+        }
+      );
+      return {
+        results: [...listOfComics],
+        offset,
+        limit,
+        total,
+        count,
+      };
+    } catch (e) {
+      console.log(e);
+    }
   }
 }
